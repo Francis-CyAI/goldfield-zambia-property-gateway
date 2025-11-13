86a80094913f43c7b356a5d7ef5ef3e0efde1b97
Francis <kalungafrancis28@gmail.com>
2025-11-13 12:54:19 +0200

    Chnaged demo notifications number to 0

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -190,7 +190,7 @@ const Header = () => {
             <Button variant="ghost" size="icon" className="relative">
               <Bell className="h-4 w-4" />
               <Badge className="absolute -top-1 -right-1 h-5 w-5 rounded-full p-0 flex items-center justify-center text-xs">
-                3
+                0
               </Badge>
             </Button>
           )}
